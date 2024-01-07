f3db8fd38c659bcceb2ede8f43da121d4780d2dd
Kevin Manjarrez <keazmanjarrezma@ittepic.edu.mx>
2024-01-07 16:43:23 -0700

    Validaciones de registrarse

--- src/app/pages/registrarse/registrarse.component.ts
+++ src/app/pages/registrarse/registrarse.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { NavComponent } from '../../shared/nav/nav.component';
 import { ApiProvider } from '../../providers/api.prov'
 import { CommonModule } from '@angular/common';
@@ -7,31 +7,43 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-registrarse',
   standalone: true,
-  imports: [FormsModule, NavComponent, CommonModule],
+  imports: [NavComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './registrarse.component.html',
   styleUrl: './registrarse.component.css'
 })
 export class RegistrarseComponent {
-  constructor(
-    private apiProv: ApiProvider
-  ) {
-    
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(public apiProv:ApiProvider, public formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      username: ['', Validators.required]
+    });
   }
+
   email: string = ''; 
   password: string = ''; 
   username:string='';
 
   public register(){
-    const data = {
-      email: this.email,
-      password: this.password,
-      userName: this.username
+    this.submitted = true;
+    if(this.registerForm.valid){
+      const emailControl = this.registerForm.get('email');
+      const passwordControl = this.registerForm.get('password');
+      const usernameControl = this.registerForm.get('username');
+      if(emailControl && passwordControl && usernameControl){
+        const data = {
+          email: emailControl.value,
+          password: passwordControl.value,
+          userName: usernameControl.value
+        }
+        this.apiProv.register(data).then(res => {
+          console.log(res);
+          window.location.href='/login';
+          }
+        );
+      }
     }
-
-    this.apiProv.register(data).then(res => {
-      console.log(res);
-      window.location.href = '/login';
-    });
   }
-
 }
