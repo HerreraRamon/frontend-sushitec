23401eef87aaf722d5eb1f0247a5d9f2d4d4fd88
RAMON_HERRERA <raherrerahe@ittepic.edu.mx>
2024-01-07 20:45:05 -0700

    clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistrarseComponent } from './pages/registrarse/registrarse.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
 
 export const routes: Routes = [
     {path:"",component:DashboardComponent},
     {path:"login",component:LoginComponent},
     {path:"registrarse",component:RegistrarseComponent},
     {path:"dashboard", component:DashboardComponent},
-    {path:"menu", component:MenuComponent}
+    {path:"menu", component:MenuComponent},
+    {path:"clientes", component:ClientesComponent}
 ];
